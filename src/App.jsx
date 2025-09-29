cb02d8cc81273a0ff02f93f80e744713c20b3fb3
renanramos3 <renanramos3@gmail.com>
2025-09-29 16:44:44 -0300

    Adicionei algumas animações no projeto

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,29 @@ import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
 
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 function App() {
+    useEffect(() => {
+        AOS.init({
+            duration: 1000, // duração da animação em ms
+            once: true, // anima apenas uma vez ao entrar na tela
+        });
+    }, []);
+
     return (
         <div className="App">
             <div className="Container">
                 <NavBar />
-                <section id="hero">
+                <section id="hero" data-aos="fade-right">
                     <Hero />
                 </section>
-                <section id="about">
+                <section id="about" data-aos="fade-up">
                     <AboutMe />
                 </section>
-                <section id="projects">
+                <section id="projects" data-aos="fade-up">
                     <Projects />
                 </section>
                 <Footer />
